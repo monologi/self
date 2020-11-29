a436b11967d70a9a2181cadd02bf6d59e68b210d
Wikan Wicaksono <komowicakomo@gmail.com>
2020-11-29 14:59:42 +0700

    add ig section

--- monologi-site/src/components/layout.js
+++ monologi-site/src/components/layout.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Motologi from "../../static/images/motologi.svg";
+import Motologi from "../../static/images/motologi.png";
 
 const Layout = (props) => {
     const { children } = props;
     return (
         <div className={`layout`} >
             <div className={`layout-head`}>
-                <img src={Motologi} alt="motologi logo" />
+                <img style={{ height: 40 }} src={Motologi} alt="motologi logo" />
                 <div className={`layout-contact-us`} >
                     <a href="mailto:motologi:motologi@gmail.com">Contact Us</a>
                 </div>
--- monologi-site/src/pages/index.js
+++ monologi-site/src/pages/index.js
@@ -1,10 +1,19 @@
 import React from "react"
 import Layout from "../components/layout";
+import instagramImg from "../../static/json/test.json";
 
 export default function Home() {
+  console.log(instagramImg)
   return (
     <Layout>
       <img style={{ width: '100%' }} src="https://drive.google.com/uc?export=view&id=1O78HJtHKwMiB37OCWqWaRw9sMStZjidf" alt="" />
+      <div className="ig-place">
+        {
+          instagramImg && instagramImg.map((item, key) => {
+            return <img className="insta-photo" src={item} alt="key" key={key} />
+          })
+        }
+      </div>
     </Layout>
   )
 }
